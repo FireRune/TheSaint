2f9781cbbe8acf7831a8a6275e1463d89318e882
FireRune <46537748+FireRune@users.noreply.github.com>
2025-09-20 22:08:26 +0200

    add EID entry for 'Mending Heart'

--- EIDRegistry.lua
+++ EIDRegistry.lua
@@ -1,8 +1,11 @@
 local registry = include("ItemRegistry")
 
 if EID then
-    local desc = ""
+	local desc = ""
 
 	desc = "Invokes the effects of 2 'book'-items.#Can also invoke Books that haven't been unlocked yet."
 	EID:addCollectible(registry.COLLECTIBLE_ALMANACH, desc)
+
+	desc = "Entering a new floor will replace 1{{BrokenHeart}} Broken Heart with 1{{EmptyHeart}} empty Heart Container.#Will replace 2 instead, if no damage was taken on the previous floor."
+	EID:addCollectible(registry.COLLECTIBLE_MENDING_HEART, desc)
 end
