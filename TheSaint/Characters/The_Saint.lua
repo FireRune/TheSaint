211ef18b922e33e6bb68b782a94be415fdca9fed
FireRune <46537748+FireRune@users.noreply.github.com>
2025-10-16 07:40:26 +0200

    finished 'Birthright' effect for 'The Saint'

--- TheSaint/Characters/The_Saint.lua
+++ TheSaint/Characters/The_Saint.lua
@@ -7,51 +7,30 @@ local game = Game()
 local char = Isaac.GetPlayerTypeByName(stats.default.name, false)
 
 local v = {
-    run = {}
+    level = {
+        angelRoomFirstEntry = true
+    }
 }
 
---- @param player EntityPlayer
-local function getPlayerCounters(player)
-    local playerIndex = "The_Saint_Counters_"..isc:getPlayerIndex(player)
-    if (not v.run[playerIndex]) then
-        v.run[playerIndex] = {
-            damage = 0,
-            fireRate = 0,
-            speed = 0,
-            range = 0
-        }
-    end
-    return v.run[playerIndex]
-end
-
---- @param player EntityPlayer
-local function getLowestStat(player)
-    --[[
-    stat values: (lowest | default | max)
-    Damage =    ?.?? | 3.50 | inf
-    Fire Rate = ?.?? | 2.73 | 5.00
-    Speed =     0.10 | 1.00 | 2.00
-    Range =     1.00 | 6.50 | inf
-    ]]
-end
-
---- Birthright effect: when entering an Angel Room increases lowest stat.<br>
---- Possible stats include: +1 Damage, +0.5 Fire Rate, +0.2 Speed, +2.5 Range
+--- Birthright effect: when entering an Angel Room increases lowest stat (by giving and removing 'Consolation Prize')
 --- @param room RoomType
 local function postNewRoomReordered_Saint_Birthright(_, room)
-    for i = 0, game:GetNumPlayers() - 1 do
-        local player = Isaac.GetPlayer(i)
-        if (player:GetPlayerType() == char)
-        and (player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT)) then
-            local counters = getPlayerCounters(player)
-            local lowestStat = getLowestStat(player)
+    if (v.level.angelRoomFirstEntry) then
+        for i = 0, game:GetNumPlayers() - 1 do
+            local player = Isaac.GetPlayer(i)
+            if (player:GetPlayerType() == char)
+            and (player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT)) then
+                player:AddCollectible(CollectibleType.COLLECTIBLE_CONSOLATION_PRIZE)
+                player:RemoveCollectible(CollectibleType.COLLECTIBLE_CONSOLATION_PRIZE)
+            end
         end
+        v.level.angelRoomFirstEntry = false
     end
 end
 
 --- @param mod ModReference
 function The_Saint:Init(mod)
-    --mod:saveDataManager("The_Saint", v)
+    mod:saveDataManager("The_Saint", v)
     mod:AddCallbackCustom(isc.ModCallbackCustom.POST_NEW_ROOM_REORDERED, postNewRoomReordered_Saint_Birthright, RoomType.ROOM_ANGEL)
 end
 
--- TheSaint/EIDRegistry.lua
+++ TheSaint/EIDRegistry.lua
@@ -16,6 +16,10 @@ if EID then
 	desc = "Entering a new floor will replace 1{{BrokenHeart}} Broken Heart with 1{{EmptyHeart}} empty Heart Container#Will replace 2 instead, if no damage was taken on the previous floor"
 	EID:addCollectible(registry.COLLECTIBLE_MENDING_HEART, desc)
 
+	local char = Isaac.GetPlayerTypeByName(stats.default.name, false)
+	desc = "{{AngelRoom}} Entering an Angel Room for the first time each floor has the following effects:#\1{{IND}} Increases one of the following stats, whichever is lowest:#\1{{IND}}{{IND}} +1 Damage#\1{{IND}}{{IND}} +0.5 Fire Rate#\1{{IND}}{{IND}} +0.2 Speed#\1{{IND}}{{IND}} +2.5 Range#{{IND}} Spawns either 3 {{Coin}} coins, 1 {{Bomb}} bomb or 1 {{Key}} key depending on what you have the least of"
+	EID:addBirthright(char, desc, "The Saint")
+
 	local taintedChar = Isaac.GetPlayerTypeByName(stats.tainted.name, true)
 	desc = "Can't use {{SoulHeart}} Soul Hearts#When you take damage, turns all {{EmptyHeart}} empty Heart Containers into {{BrokenHeart}} Broken Hearts (doesn't apply to self-damage)#{{Collectible"..registry.COLLECTIBLE_MENDING_HEART.."}} Entering a new floor will replace 1{{BrokenHeart}} Broken Heart with 1{{EmptyHeart}} empty Heart Container#{{Collectible"..registry.COLLECTIBLE_MENDING_HEART.."}} Will replace 2 instead, if no damage was taken on the previous floor"
 	EID:addCharacterInfo(taintedChar, desc, "The Saint")
