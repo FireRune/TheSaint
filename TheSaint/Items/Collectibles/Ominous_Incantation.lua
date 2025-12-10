fd143da2a9f652ab2fc59e93708caf528c2dc812
FireRune <46537748+FireRune@users.noreply.github.com>
2025-12-10 17:51:18 +0100

    continue working on "Ominous Incantation

--- TheSaint/Items/Collectibles/Ominous_Incantation.lua
+++ TheSaint/Items/Collectibles/Ominous_Incantation.lua
@@ -1,108 +1,269 @@
-local isc = require("TheSaint.lib.isaacscript-common")
-local enums = require("TheSaint.Enums")
-local PlayerLoadout = require("TheSaint.classes.PlayerLoadout")
+-- if REPENTOGON then
 
---- @class TheSaint.Items.Collectibles.Ominous_Incantation : TheSaint_Feature
-local Ominous_Incantation = {
-	IsInitialized = false,
-	FeatureSubType = enums.CollectibleType.COLLECTIBLE_OMINOUS_INCANTATION,
-	SaveDataKey = "Ominous_Incantation",
-}
+-- 	--#region Repentogon
 
---- @type ModUpgraded
-local thisMod
+-- 	local isc = require("TheSaint.lib.isaacscript-common")
+-- 	local enums = require("TheSaint.Enums")
+-- 	local PlayerLoadout = require("TheSaint.classes.PlayerLoadout")
 
-local RevivalState = {
-	NONE = 0,
-	USE_GENESIS = 1,
-	USE_R_KEY = 2,
-}
+-- 	--- (Repentogon version)
+-- 	--- - Grants an extra life.
+-- 	--- - Player will revive in the starting room of the current floor and lose the following:
+-- 	---   - all passive and active items (except innate passives)
+-- 	---   - all held trinkets
+-- 	---   - all held pocket items (except pocket actives)
+-- 	--- - Player will also be in Lost form (i.e. white fireplace in Downpour II/Dross II).
+-- 	--- - Spawns a special enemy in the room the player had died.
+-- 	---   - Killing it restores the player to their original form and gives back their items
+-- 	--- - Works as a normal extra life when fighting "The Beast"
+-- 	--- @class TheSaint.Items.Collectibles.Ominous_Incantation : TheSaint_Feature
+-- 	local Ominous_Incantation = {
+-- 		IsInitialized = false,
+-- 		FeatureSubType = enums.CollectibleType.COLLECTIBLE_OMINOUS_INCANTATION,
+-- 		SaveDataKey = "Ominous_Incantation",
+-- 	}
 
-local currentRevivalState = RevivalState.NONE
---- @type EntityPlayer?
-local currentRevivalPlayer = nil
+-- 	--- @type ModUpgraded
+-- 	local thisMod
 
-local v = {
-	run = {
-		--- @type table<string, SerializablePlayerLoadout>
-		PlayerLoadouts = {},
+-- 	local RevivalState = {
+-- 		NONE = 0,
+-- 		USE_GENESIS = 1,
+-- 		USE_R_KEY = 2,
+-- 	}
+
+-- 	local currentRevivalState = RevivalState.NONE
+-- 	--- @type EntityPlayer?
+-- 	local currentRevivalPlayer = nil
+
+-- 	local v = {
+-- 		run = {
+-- 			--- @type table<string, SerializablePlayerLoadout>
+-- 			PlayerLoadouts = {},
+-- 		}
+-- 	}
+
+-- 	--- Save the given player's current loadout
+-- 	--- @param player EntityPlayer
+-- 	local function saveLoadout(player)
+-- 		local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
+
+-- 		local loadout = PlayerLoadout.createFromPlayer(thisMod, player)
+
+-- 		v.run.PlayerLoadouts[playerIndex] = loadout:serialize()
+-- 	end
+
+-- 	--- @param player EntityPlayer
+-- 	--- @return SerializablePlayerLoadout
+-- 	local function getLoadout(player)
+-- 		local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
+-- 		return v.run.PlayerLoadouts[playerIndex]
+-- 	end
+
+-- 	--- @param player EntityPlayer
+-- 	local function preCustomRevive(_, player)
+-- 		if (player:HasCollectible(Ominous_Incantation.FeatureSubType)) then
+-- 			return Ominous_Incantation.FeatureSubType
+-- 		end
+-- 		return nil
+-- 	end
+
+-- 	--- @param player EntityPlayer
+-- 	--- @param revivalType integer
+-- 	local function postCustomRevive(_, player, revivalType)
+-- 		player:RemoveCollectible(Ominous_Incantation.FeatureSubType)
+
+-- 		-- during "The Beast" fight, functions as a simple extra life
+-- 		if (isc:inBeastRoom()) then
+-- 			player:AnimateCollectible(Ominous_Incantation.FeatureSubType)
+-- 		else
+-- 			saveLoadout(player)
+-- 			currentRevivalPlayer = player
+-- 			currentRevivalState = RevivalState.USE_GENESIS
+-- 			isc:useActiveItemTemp(player, CollectibleType.COLLECTIBLE_GENESIS)
+-- 		end
+-- 	end
+
+-- 	--- @param room RoomType
+-- 	local function postNewRoomEarly(_, room)
+-- 		if (currentRevivalState == RevivalState.USE_GENESIS) then
+-- 			currentRevivalState = RevivalState.USE_R_KEY
+-- 			isc:useActiveItemTemp(currentRevivalPlayer, CollectibleType.COLLECTIBLE_R_KEY)
+-- 		end
+-- 	end
+
+-- 	--- @param stage LevelStage
+-- 	--- @param stageType StageType
+-- 	local function postNewLevelReordered(_, stage, stageType)
+-- 		if (currentRevivalState == RevivalState.USE_R_KEY) then
+-- 			currentRevivalState = RevivalState.NONE
+-- 			if (currentRevivalPlayer) then
+-- 				currentRevivalPlayer:AnimateCollectible(Ominous_Incantation.FeatureSubType)
+-- 				currentRevivalPlayer = nil
+-- 			end
+-- 		end
+-- 	end
+
+-- 	--- @param mod ModUpgraded
+-- 	function Ominous_Incantation:Init(mod)
+-- 		if (self.IsInitialized) then return end
+
+-- 		thisMod = mod
+
+-- 		mod:saveDataManager(self.SaveDataKey, v)
+
+-- 		-- want to run this callback pretty late, so using priority of 1000
+-- 		mod:AddPriorityCallbackCustom(isc.ModCallbackCustom.PRE_CUSTOM_REVIVE, 1000, preCustomRevive, 0)
+-- 		mod:AddCallbackCustom(isc.ModCallbackCustom.POST_CUSTOM_REVIVE, postCustomRevive, self.FeatureSubType)
+-- 		mod:AddCallbackCustom(isc.ModCallbackCustom.POST_NEW_ROOM_EARLY, postNewRoomEarly)
+-- 		mod:AddCallbackCustom(isc.ModCallbackCustom.POST_NEW_LEVEL_REORDERED, postNewLevelReordered)
+-- 	end
+
+-- 	return Ominous_Incantation
+
+-- 	--#endregion
+
+-- else
+
+	--#region Vanilla
+
+	local isc = require("TheSaint.lib.isaacscript-common")
+	local enums = require("TheSaint.Enums")
+	local PlayerLoadout = require("TheSaint.classes.PlayerLoadout")
+
+	local game = Game()
+
+	--- (Vanilla version)
+	--- - Grants an extra life.
+	--- - Upon revival, player loses the following:
+	---   - all pickups (coins, bombs, keys, etc.)
+	---   - all passive and active items (except innate passives)
+	---   - all held trinkets
+	---   - all held pocket items (except pocket actives)
+	--- - Player will also be in Lost form (i.e. white fireplace in Downpour II/Dross II).
+	--- - Entering a new floor will revert the player back to their original form and gives back their lost items
+	--- - Works as a normal extra life when fighting "The Beast"
+	--- @class TheSaint.Items.Collectibles.Ominous_Incantation : TheSaint_Feature
+	local Ominous_Incantation = {
+		IsInitialized = false,
+		FeatureSubType = enums.CollectibleType.COLLECTIBLE_OMINOUS_INCANTATION,
+		SaveDataKey = "Ominous_Incantation",
 	}
-}
 
---- Save the given player's current loadout
---- @param player EntityPlayer
-local function saveLoadout(player)
-	local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
+	local v = {
+		run = {
+			--- @type table<string, SerializablePlayerLoadout>
+			PlayerLoadouts = {},
+		}
+	}
 
-	local loadout = PlayerLoadout.createFromPlayer(thisMod, player)
+	--#region PlayerLoadout helper
 
-	v.run.PlayerLoadouts[playerIndex] = loadout:serialize()
-end
-
---- @param player EntityPlayer
---- @return SerializablePlayerLoadout
-local function getLoadout(player)
-	local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
-	return v.run.PlayerLoadouts[playerIndex]
-end
-
---- @param player EntityPlayer
-local function preCustomRevive(_, player)
-	if (player:HasCollectible(Ominous_Incantation.FeatureSubType)) then
-		return Ominous_Incantation.FeatureSubType
+	--- Clears the saved loadout of the given player
+	--- @param player EntityPlayer
+	local function clearLoadout(player)
+		local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
+		v.run.PlayerLoadouts[playerIndex] = nil
 	end
-	return nil
-end
 
---- @param player EntityPlayer
---- @param revivalType integer
-local function postCustomRevive(_, player, revivalType)
-	player:RemoveCollectible(Ominous_Incantation.FeatureSubType)
+	--- Gets the saved loadout for the given player, or `nil` if it doesn't exist
+	--- @param player EntityPlayer
+	--- @return SerializablePlayerLoadout?
+	local function getLoadout(player)
+		local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
+		return v.run.PlayerLoadouts[playerIndex]
+	end
 
-	-- during "The Beast" fight, functions as a simple extra life
-	if (isc:inBeastRoom()) then
+	--- Save the given player's current loadout
+	--- @param player EntityPlayer
+	local function saveLoadout(player)
+		local playerIndex = "OI_Loadout_"..isc:getPlayerIndex(player)
+
+		local loadout = PlayerLoadout.createFromPlayer(Ominous_Incantation.ThisMod, player)
+
+		v.run.PlayerLoadouts[playerIndex] = loadout:serialize()
+	end
+
+	--#endregion
+
+	--- @param player EntityPlayer
+	local function preCustomRevive(_, player)
+		if (player:HasCollectible(Ominous_Incantation.FeatureSubType)) then
+			return Ominous_Incantation.FeatureSubType
+		end
+		return nil
+	end
+
+	--- @param player EntityPlayer
+	--- @param revivalType integer
+	local function postCustomRevive(_, player, revivalType)
+		player:RemoveCollectible(Ominous_Incantation.FeatureSubType)
+
+		-- during "The Beast" fight, functions as a simple extra life
+		if (isc:inBeastRoom() == false) then
+			saveLoadout(player)
+			local effects = player:GetEffects()
+
+			--- adding 3 makes this effect permanent, and it must be removed manually
+			effects:AddNullEffect(NullItemID.ID_LOST_CURSE, true, 3)
+
+			local loadout = getLoadout(player)
+			if (loadout) then
+				--- Pickups
+				player:AddCoins(player:GetNumCoins())
+				player:AddGigaBombs(player:GetNumGigaBombs())
+				player:AddBombs(player:GetNumBombs())
+				player:AddKeys(player:GetNumKeys())
+				player:AddPoopMana(player:GetPoopMana())
+				player:SetSoulCharge(0)
+				player:SetBloodCharge(0)
+				--- Collectibles
+				--- @param item CollectibleType
+				isc:forEach(loadout.Collectibles, function (item)
+					player:RemoveCollectible(item)
+				end)
+				--- Trinkets
+				--- @param trinket TrinketType
+				isc:forEach(loadout.Collectibles, function (trinket)
+					player:TryRemoveTrinket(trinket)
+				end)
+				--- Cards/Pills
+				player:SetCard(0, Card.CARD_NULL)
+				player:SetCard(1, Card.CARD_NULL)
+			end
+		end
 		player:AnimateCollectible(Ominous_Incantation.FeatureSubType)
-	else
-		saveLoadout(player)
-		currentRevivalPlayer = player
-		currentRevivalState = RevivalState.USE_GENESIS
-		isc:useActiveItemTemp(player, CollectibleType.COLLECTIBLE_GENESIS)
 	end
-end
 
---- @param room RoomType
-local function postNewRoomEarly(_, room)
-	if (currentRevivalState == RevivalState.USE_GENESIS) then
-		currentRevivalState = RevivalState.USE_R_KEY
-		isc:useActiveItemTemp(currentRevivalPlayer, CollectibleType.COLLECTIBLE_R_KEY)
-	end
-end
-
---- @param stage LevelStage
---- @param stageType StageType
-local function postNewLevelReordered(_, stage, stageType)
-	if (currentRevivalState == RevivalState.USE_R_KEY) then
-		currentRevivalState = RevivalState.NONE
-		if (currentRevivalPlayer) then
-			currentRevivalPlayer:AnimateCollectible(Ominous_Incantation.FeatureSubType)
-			currentRevivalPlayer = nil
+	--- @param stage LevelStage
+	--- @param stageType StageType
+	local function postNewLevelReordered(_, stage, stageType)
+		for i = 0, game:GetNumPlayers() - 1 do
+			local player = Isaac.GetPlayer(i)
+			local loadout = getLoadout(player)
+			if (loadout) then
+				player:GetEffects():RemoveNullEffect(NullItemID.ID_LOST_CURSE, -1)
+				clearLoadout(player)
+			end
 		end
 	end
-end
 
---- @param mod ModUpgraded
-function Ominous_Incantation:Init(mod)
-	if (self.IsInitialized) then return end
+	--- @param mod ModUpgraded
+	function Ominous_Incantation:Init(mod)
+		if (self.IsInitialized) then return end
 
-	thisMod = mod
+		self.ThisMod = mod
 
-	mod:saveDataManager(self.SaveDataKey, v)
+		mod:saveDataManager(self.SaveDataKey, v)
 
-	-- want to run this callback pretty late, so using priority of 1000
-	mod:AddPriorityCallbackCustom(isc.ModCallbackCustom.PRE_CUSTOM_REVIVE, 1000, preCustomRevive, 0)
-	mod:AddCallbackCustom(isc.ModCallbackCustom.POST_CUSTOM_REVIVE, postCustomRevive, self.FeatureSubType)
-	mod:AddCallbackCustom(isc.ModCallbackCustom.POST_NEW_ROOM_EARLY, postNewRoomEarly)
-	mod:AddCallbackCustom(isc.ModCallbackCustom.POST_NEW_LEVEL_REORDERED, postNewLevelReordered)
-end
+		-- want to run this callback pretty late, so using priority of 1000
+		mod:AddPriorityCallbackCustom(isc.ModCallbackCustom.PRE_CUSTOM_REVIVE, 1000, preCustomRevive, 0)
+		mod:AddCallbackCustom(isc.ModCallbackCustom.POST_CUSTOM_REVIVE, postCustomRevive, self.FeatureSubType)
+		mod:AddCallbackCustom(isc.ModCallbackCustom.POST_NEW_LEVEL_REORDERED, postNewLevelReordered)
+	end
 
-return Ominous_Incantation
+	return Ominous_Incantation
+
+	--#endregion
+
+-- end
